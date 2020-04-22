05391b347d2752a973db7b92a5b0a3964deebd4f
PecaLee <58648931+PecaLee@users.noreply.github.com>
2020-04-22 10:34:42 +0900

    delete js

--- src/scroll.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('.button').click(function(){
-        var offset = $('#form').offset(); 
-        $('html').animate({scrollTop : offset.top}, 400);
-    });
-});
